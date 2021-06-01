fa5d6eba17a04c62135104f0252b37e2887de3fd
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-06-01 14:34:11 +0100

    Added database context

---
+++ CateringSystemWeb/Data/CateringContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+using CateringSystem;
+
+namespace CateringSystemWeb.Data
+{
+    public class CateringContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public CateringContext() { }
+        public CateringContext(DbContextOptions<CateringContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CateringSystem-2afded41-3b4c-47f2-8dfe-17ed883bc15b;Trusted_Connection=True");
+        }
+    }
+}
