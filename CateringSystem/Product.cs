df6994038eb7a0e759ace94260059a1a26752dc2
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-06-01 14:32:33 +0100

    Adjusted string sizes

--- CateringSystem/Product.cs
+++ CateringSystem/Product.cs
@@ -3,17 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CateringSystem
 {
-    public class Product : IEquatable<Product>
+    public class Product
     {
         [Key]
         public int ID { get; set; }
 
         [Required]
-        [StringLength(30)]
+        [StringLength(63)]
         public String Name { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [StringLength(255)]
         public String Description { get; set; }
 
         [DataType(DataType.Currency)]
@@ -27,13 +27,5 @@ namespace CateringSystem
         public int CategoryId { get; set; }
 
         public virtual Category Category { get; set; }
-
-        public override bool Equals(Object obj) => this.Equals(obj as Product);
-        public bool Equals(Product product) => !(product is null) && this.ID.Equals(product.ID);
-        public override int GetHashCode() => this.ID;
-        public override String ToString() => this.Name;
-
-        public static bool operator ==(Product a, Product b) => a is null ? b is null : a.Equals(b);
-        public static bool operator !=(Product a, Product b) => !(a == b);
     }
 }
