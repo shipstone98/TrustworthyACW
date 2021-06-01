54b12e59ebb3e9ae68e20467af8913a192652a0a
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-06-01 14:34:31 +0100

    Added seed data

---
+++ SQL Queries/Categories Seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO [CateringSystem-2afded41-3b4c-47f2-8dfe-17ed883bc15b].[dbo].[Categories] VALUES (
+    'Hot Beverages',
+    'Contains hot drinks such as coffee, tea and hot chocolate.'
+), (
+    'Cold Beverages',
+    'Contains cold drinks such as fizzy drinks, fresh fruit juice and water.'
+), (
+    'Hot Food',
+    'Contains hot food such as home-made dishes, microwavable meals and paninis.'
+), (
+    'Cold Food',
+    'Contains cold food such as sandwiches, wraps and buffet.'
+), (
+    'Snacks',
+    'Contains snacks such as crisps, biscuits and fruit.'
+)
---
+++ SQL Queries/Products Seed.sql
@@ -0,0 +1,61 @@
+INSERT INTO [CateringSystem-2afded41-3b4c-47f2-8dfe-17ed883bc15b].[dbo].[Products] VALUES (
+    'Coffee',
+    'A brewed drink made from roasted coffee beans.',
+    1.50,
+    'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG',
+    1
+), (
+    'Tea',
+    'An aromatic beverage containing tea leaves boiled in water.',
+    1.50,
+    'https://upload.wikimedia.org/wikipedia/commons/d/d3/Longjing_tea_steeping_in_gaiwan.jpg',
+    1
+), (
+    'Orange Juice',
+    'Liquid extract produced by squeezing the orange fruit.',
+    0.90,
+    'https://upload.wikimedia.org/wikipedia/commons/5/5a/Oranges_and_orange_juice.jpg',
+    2
+), (
+    'Sparkling Water',
+    'Water containing dissolved carbon dioxide gas.',
+    0.60,
+    'https://upload.wikimedia.org/wikipedia/commons/5/59/Drinking_glass_00118.gif',
+    2
+), (
+    'Jacket Potato',
+    'A type of potato dish with a fluffy interior and a crisp skin. Served with fillings.',
+    4.50,
+    'https://upload.wikimedia.org/wikipedia/commons/9/93/BakedPotatoWithButter.jpg',
+    3
+), (
+    'Pizza',
+    'A savory dish of Italian origin consisting of a round, flattened base of dough topped with tomatoes and cheese.',
+    6.00,
+    'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+    3
+), (
+    'Tuna Wrap',
+    'Filleted tuna chunks in a wholemeal wrap with mayonnaise.',
+    3.50,
+    'https://www.culinaryhill.com/wp-content/uploads/2020/08/The-Best-Tuna-Salad-Culinary-Hill-hero.jpg',
+    4
+), (
+    'Philly Cheesesteak',
+    'A sandwich made from thinly sliced pieces of beef steak and melted cheese in a long hoagie roll.',
+    4.50,
+    'https://upload.wikimedia.org/wikipedia/commons/f/f1/PatsCheesesteak.jpg',
+    4
+), (
+    'Salt & Vinegar Crisps',
+    'Baked potato slices with salt and vinegar seasoning.',
+    1.00,
+    'https://groceries.morrisons.com/productImages/372/372818011_0_1280x1280.jpg?identifier=dc685c9b994e120532777cd6111d1854',
+    5
+), (
+    'Single Banana',
+    'A sweet, soft fruit.',
+    0.60,
+    'https://upload.wikimedia.org/wikipedia/commons/f/ff/Banana_and_cross_section.jpg',
+    5
+)
