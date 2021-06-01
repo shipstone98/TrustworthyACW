1d7db447db8ae725599b59838b59e7ffa8a85d6c
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-06-01 15:39:33 +0100

    Moved cart to class lib

---
+++ CateringSystem/Cart.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CateringSystem
+{
+    public class Cart
+    {
+        private readonly ICartHandler _Handler;
+        private readonly String _ID;
+
+        public IEnumerable<CartItem> Items => this._Handler.Items;
+
+        public Cart(ICartHandler handler)
+        {
+            if (handler is null)
+            {
+                throw new ArgumentNullException(nameof (handler));
+            }
+
+            if (String.IsNullOrWhiteSpace(handler.ID))
+            {
+                throw new NotImplementedException();
+            }
+
+            this._Handler = handler;
+            this._ID = handler.ID;
+        }
+
+        public async Task AddAsync(int productId)
+        {
+            Product product = this._Handler.GetProduct(productId);
+            CartItem cartItem = this.Items.FirstOrDefault(item => item.ProductId == productId);
+
+            if (cartItem is null)
+            {
+                cartItem = new CartItem
+                {
+                    CartId = this._ID,
+                    Product = product,
+                    Quantity = 1
+                };
+
+                await this._Handler.AddCartItemAsync(cartItem);
+            }
+
+            else
+            {
+                ++ cartItem.Quantity;
+            }
+
+            await this._Handler.SaveChangesAsync();
+        }
+    }
+}
---
+++ CateringSystemWeb/Data/CartHandler.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+using CateringSystem;
+
+namespace CateringSystemWeb.Data
+{
+    internal class CartHandler : ICartHandler
+    {
+        private readonly CateringContext _DbContext;
+        private readonly HttpContext _HttpContext;
+
+        public String ID { get; }
+        public IEnumerable<CartItem> Items => this._DbContext.CartItems.Where(item => item.CartId.Equals(this.ID));
+
+        public CartHandler(CateringContext dbContext, HttpContext httpContext)
+        {
+            if (dbContext is null)
+            {
+                throw new ArgumentNullException(nameof (dbContext));
+            }
+
+            if (httpContext is null)
+            {
+                throw new ArgumentNullException(nameof (httpContext));
+            }
+
+            this._DbContext = dbContext;
+            this._HttpContext = httpContext;
+            this.ID = httpContext.Session.Id;
+            Nullable<int> sessionInit = httpContext.Session.GetInt32(DatabaseAccessor._SessionInitKey);
+
+            if (!sessionInit.HasValue || sessionInit.Value == 0)
+            {
+                httpContext.Session.SetInt32(DatabaseAccessor._SessionInitKey, 1);
+            }
+        }
+
+        public async Task AddCartItemAsync(CartItem cartItem) => await this._DbContext.AddAsync(cartItem);
+        public Product GetProduct(int productId) => this._DbContext.GetProduct(productId);
+        public async Task SaveChangesAsync() => await this._DbContext.SaveChangesAsync();
+    }
+}
