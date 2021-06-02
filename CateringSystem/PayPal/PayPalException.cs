bfd53e365aa6cfcbef97ac68753c57ce70d4cf05
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-06-02 13:27:23 +0100

    Added message

--- CateringSystem/PayPal/PayPalException.cs
+++ CateringSystem/PayPal/PayPalException.cs
@@ -6,6 +6,7 @@ namespace CateringSystem.PayPal
     public class PayPalException : Exception
     {
         internal const int _GetTokenError = 1000;
+        internal const int _OrderCreationError = 1001;
 
         private static readonly IReadOnlyDictionary<int, String> _ErrorMessages;
 
@@ -13,7 +14,8 @@ namespace CateringSystem.PayPal
 
         static PayPalException() => PayPalException._ErrorMessages = new Dictionary<int, String>
         {
-            { PayPalException._GetTokenError, "Error occurred when retrieving an access token" }
+            { PayPalException._GetTokenError, "Error occurred when retrieving an access token" },
+            { PayPalException._OrderCreationError, "Error occurred when creating an order request" }
         };
 
         internal PayPalException(int id, bool isSandbox) : base(PayPalException._ErrorMessages[id]) => this.IsSandbox = isSandbox;
