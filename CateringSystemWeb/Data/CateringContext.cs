5c0ad5b02d244d862e559f648c5b1c5d7c123ed1
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-06-09 13:23:50 +0100

    Added multiple active sets

--- CateringSystemWeb/Data/CateringContext.cs
+++ CateringSystemWeb/Data/CateringContext.cs
@@ -18,7 +18,7 @@ namespace CateringSystemWeb.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CateringSystem-2afded41-3b4c-47f2-8dfe-17ed883bc15b;Trusted_Connection=True;MultipleActiveResultSets=true;");
-            optionsBuilder.UseSqlServer("Server=tcp:cateringsystemdb.database.windows.net,1433;Database=coreDB;User ID=chris;Password=Carrot31;Encrypt=true;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer("Server=tcp:cateringsystemdb.database.windows.net,1433;Database=coreDB;User ID=chris;Password=Carrot31;Encrypt=true;Connection Timeout=30;MultipleActiveResultSets=true;");
         }
     }
 }
